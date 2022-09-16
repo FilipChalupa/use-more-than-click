abf21e09eaf3075b8996b7f30ad1898060e42d6d
Filip Chalupa <chalupa.filip@gmail.com>
2022-09-17 00:09:45 +0200

    Add hook placeholder

--- src/stories/Example.tsx
+++ src/stories/Example.tsx
@@ -1,5 +1,15 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useCallback, useRef } from 'react'
+import { MoreThanClickCallback, useMoreThanClick } from '../useMoreThanClick'
 
 export const Example: FunctionComponent = () => {
-	return <button>Click me</button>
+	const ref = useRef<HTMLButtonElement>(null)
+	const handleDoubleClickOrLongPress = useCallback<MoreThanClickCallback>(
+		(type) => {
+			alert(`User performed ${type}.`)
+		},
+		[],
+	)
+	useMoreThanClick(ref, handleDoubleClickOrLongPress)
+
+	return <button ref={ref}>Click me</button>
 }
--- src/useMoreThanClick.ts
+++ src/useMoreThanClick.ts
@@ -1,1 +1,28 @@
-export const useMoreThanClick = () => {}
+import { RefObject, useCallback, useEffect } from 'react'
+
+export type MoreThanClickCallback = (
+	type: 'double-click' | 'long-press',
+) => void
+
+export const useMoreThanClick = (
+	ref: RefObject<HTMLElement>,
+	callback: MoreThanClickCallback,
+) => {
+	const handleClick = useCallback(() => {
+		callback('double-click') // @TODO: this is not double click yet
+	}, [])
+
+	useEffect(() => {
+		const element = ref.current
+		if (element === null) {
+			return
+		}
+
+		// @TODO: handle double click and long press
+		element.addEventListener('click', handleClick)
+
+		return () => {
+			element.removeEventListener('click', handleClick)
+		}
+	}, [ref, handleClick])
+}
