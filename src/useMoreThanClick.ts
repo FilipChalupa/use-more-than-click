01e799e857b0cf2133be0b003035a6466e15b06e
Filip Chalupa <chalupa.filip@gmail.com>
2022-09-24 18:36:38 +0200

    Hook: add rough action type distinction

--- src/useMoreThanClick.ts
+++ src/useMoreThanClick.ts
@@ -1,18 +1,82 @@
-import { RefObject, useCallback, useEffect, useMemo, useState } from 'react'
+import {
+	RefObject,
+	useCallback,
+	useEffect,
+	useMemo,
+	useRef,
+	useState,
+} from 'react'
 
-export type MoreThanClickCallback = (
-	type: 'double-click' | 'long-press',
-) => void
+const firstSingleClickProgress = 0.3
+const minimumHoldDurationMilliseconds = 300
+const holdDurationToActionMilliseconds = 800
+
+const getNow = () => Date.now()
+
+export type MoreThanClickType = 'double-click' | 'long-press'
+
+export type MoreThanClickCallback = (type: MoreThanClickType) => void
 
 export const useMoreThanClick = (
 	ref: RefObject<HTMLElement>,
 	actionHandler: MoreThanClickCallback,
 ) => {
 	const [progress, setProgress] = useState(0)
+	const actionTypeRef = useRef<MoreThanClickType>(
+		'double-click' /* random placeholder value */,
+	)
+	const [holdStart, setHoldStart] = useState({
+		progress: 0,
+		time: 0,
+	})
+	const [holdEnd, setHoldEnd] = useState(0)
+	const [isHolding, setIsHolding] = useState(false)
+
+	const triggerActionHandler = useCallback(
+		(type: MoreThanClickType) => {
+			setProgress(0) // @TODO: delay/decay maybe
+			actionHandler(type)
+		},
+		[actionHandler],
+	)
+
 	const handleClick = useCallback(() => {
-		actionHandler('double-click') // @TODO: this is not double click yet
+		console.log('click')
+		if (holdEnd - holdStart.time >= minimumHoldDurationMilliseconds) {
+			if (progress === 1) {
+				triggerActionHandler('long-press')
+			}
+			return
+		}
+		if (progress < firstSingleClickProgress / 2) {
+			setProgress(firstSingleClickProgress)
+			return
+		}
 		setProgress(1)
-	}, [actionHandler])
+		triggerActionHandler('double-click')
+	}, [
+		actionTypeRef,
+		actionHandler,
+		holdEnd,
+		holdStart,
+		triggerActionHandler,
+		progress,
+	])
+
+	const handlePointerDown = useCallback(() => {
+		console.log('down')
+		setHoldStart({ progress, time: getNow() })
+		setIsHolding(true)
+	}, [progress])
+
+	const handlePointerUp = useCallback(() => {
+		console.log('up')
+		if (!isHolding) {
+			return
+		}
+		setHoldEnd(getNow())
+		setIsHolding(false)
+	}, [isHolding])
 
 	useEffect(() => {
 		const element = ref.current
@@ -20,13 +84,44 @@ export const useMoreThanClick = (
 			return
 		}
 
-		// @TODO: handle double click and long press
 		element.addEventListener('click', handleClick)
+		element.addEventListener('pointerdown', handlePointerDown)
+		element.addEventListener('pointerup', handlePointerUp) // @TODO: handle pointer leave/cancel
 
 		return () => {
 			element.removeEventListener('click', handleClick)
+			element.removeEventListener('pointerdown', handlePointerDown)
+			element.removeEventListener('pointerup', handlePointerUp)
 		}
-	}, [ref, handleClick])
+	}, [ref, handleClick, handlePointerDown, handlePointerUp])
+
+	// @TODO: add decay
+
+	useEffect(() => {
+		if (!isHolding) {
+			return
+		}
+
+		let callback: ReturnType<typeof requestAnimationFrame>
+		const loop = () => {
+			console.log('loop')
+			setProgress((progress) => {
+				const now = getNow()
+				console.log({ progress, start: holdStart.time, now })
+				return Math.min(
+					1,
+					holdStart.progress +
+						(now - holdStart.time) / holdDurationToActionMilliseconds,
+				)
+			})
+			callback = requestAnimationFrame(loop)
+		}
+		loop()
+
+		return () => {
+			cancelAnimationFrame(callback)
+		}
+	}, [isHolding, holdStart])
 
 	return useMemo(() => ({ progress }), [progress])
 }
