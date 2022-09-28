165804183f81d4443f960f2c8c0f36cc7fb7bc47
Filip Chalupa <chalupa.filip@gmail.com>
2022-09-28 17:14:11 +0200

    Demo: add confetti

--- src/stories/Example.tsx
+++ src/stories/Example.tsx
@@ -3,30 +3,50 @@ import React, {
 	FunctionComponent,
 	useCallback,
 	useRef,
+	useState,
 } from 'react'
+import { useReward } from 'react-rewards'
 import { MoreThanClickCallback, useMoreThanClick } from '../useMoreThanClick'
 
 export const Example: FunctionComponent = () => {
 	const ref = useRef<HTMLButtonElement>(null)
+	const { reward } = useReward('confettiReward', 'confetti')
+	const [actionInfo, setActionInfo] = useState({
+		text: '',
+		key: 0,
+	})
 	const handleDoubleClickOrLongPress = useCallback<MoreThanClickCallback>(
 		(type) => {
-			alert(`User performed ${type}.`)
+			reward()
+			setActionInfo({ text: `User performed ${type}.`, key: Math.random() })
 		},
 		[],
 	)
 	const { progress } = useMoreThanClick(ref, handleDoubleClickOrLongPress)
 
 	return (
-		<button
-			className="example-button"
-			style={
-				{
-					['--progress']: progress,
-				} as CSSProperties
-			}
-			ref={ref}
-		>
-			Click me
-		</button>
+		<>
+			<h2>Example button</h2>
+			<p>Press twice or give it one long press.</p>
+			<div className="example-button-wrapper">
+				<div className="example-button-confetti" id="confettiReward" />
+				<button
+					className="example-button"
+					style={
+						{
+							['--progress']: progress,
+						} as CSSProperties
+					}
+					ref={ref}
+				>
+					Click me
+				</button>
+			</div>
+			{actionInfo && (
+				<p key={actionInfo.key} className="example-button-action">
+					{actionInfo.text}
+				</p>
+			)}
+		</>
 	)
 }
